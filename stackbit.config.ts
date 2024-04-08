9367448f53b0890a65440876204c9f6aac293026
aguimael <aguimael.rocha@gmail.com>
2024-04-08 06:11:04 -0300

    novo doc

--- stackbit.config.ts
+++
@@ -1,5 +0,0 @@
-export default defineStackbitConfig({
-    contentSources: [ /* ... */ ]
-    modelExtensions: [{ name: 'page', type: 'page', urlPath: '/{slug}' }],
-    // other properties ...
-  })
